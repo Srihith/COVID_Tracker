624f451101da1a354a4d91778c9b5a6b4f843fd5
Srihith Maraka <srihith.maraka@gmail.com>
2020-10-06 15:47:38 -0400

    a
    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GlobalComponent } from './global/global.component';
+import { MeComponent } from './me/me.component';
+import { FriendsComponent } from './friends/friends.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'app-me', component: MeComponent},
+  {path: 'app-friends', component: FriendsComponent},
+  {path: 'app-global', component: GlobalComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GlobalComponent } from './global/global.component';
+import { MeComponent } from './me/me.component';
+import { FriendsComponent } from './friends/friends.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from  '@angular/material/toolbar';
+import { MatButtonModule } from  '@angular/material/button';
+import { MatIconModule } from  '@angular/material/icon';
+import { MatSidenavModule } from  '@angular/material/sidenav';
+import { MatListModule } from  '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GlobalComponent,
+    MeComponent,
+    FriendsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
