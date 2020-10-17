9260dc213b65f9b6601c142c6f10a8d674d08774
Srihith Maraka <srihith.maraka@gmail.com>
2020-10-17 03:07:39 -0400

    added bar graph

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GlobalComponent } from './global/global.component';
@@ -13,14 +13,15 @@ import { MatButtonModule } from  '@angular/material/button';
 import { MatIconModule } from  '@angular/material/icon';
 import { MatSidenavModule } from  '@angular/material/sidenav';
 import { MatListModule } from  '@angular/material/list';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
     AppComponent,
     GlobalComponent,
     MeComponent,
-    FriendsComponent
+    FriendsComponent,
   ],
   imports: [
     BrowserModule,
@@ -33,7 +34,8 @@ import {MatGridListModule} from '@angular/material/grid-list';
     MatListModule,
     HttpClientModule,
     MatCardModule,
-    MatGridListModule
+    MatGridListModule,
+    ChartsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/global/global.component.ts
+++ src/app/global/global.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {MatCardModule} from '@angular/material/card';
 import {MatGridListModule} from '@angular/material/grid-list';
+import { ChartsModule } from 'ng2-charts';
 
 @Component({
   selector: 'app-global',
@@ -13,9 +14,31 @@ export class GlobalComponent implements OnInit {
 
   constructor(private http: HttpClient) { }
 
+  public barChartOptions = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+
+  public barChartLabels = [];
+  deaths=[];
+  public barChartType = 'bar';
+  public barChartLegend = true;
+  public barChartData = [
+    {data: this.deaths, label: 'Deaths'},
+  ];
+
   ngOnInit(): void {
     this.http.get<covidAPIData>('https://api.covidtracking.com/v1/us/daily.json').subscribe(data => {
         this.covidData = data;
+        for(let i=0;i<=7;i++){
+          var fullDate = this.covidData[i].date;
+          var year = String(fullDate).substring(0,4);
+          var month = String(fullDate).substring(4,6);
+          var day = String(fullDate).substring(6,8);
+          var currDate = month + "-" + day + "-" + year;
+          this.barChartLabels.push(currDate);
+          this.deaths.push(this.covidData[i].death);
+        }
     });
   }
 }
