e09e3f977565077dcc1e6388816ea99ea644b904
notmattguz <mag369@pitt.edu>
2020-10-26 01:40:38 -0400

    basic friend funcationality
    its not pretty but it adds, stores, and lists friends

--- src/app/friends/friends.component.ts
+++ src/app/friends/friends.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-friends',
   templateUrl: './friends.component.html',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FriendsComponent implements OnInit {
 
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
+	friendlist = new Array();
+	num = 0;
+	friend = "";
+	
+	handleClick()
+	{
+		this.friendlist.push(this.friend);		
+		num = num + 1;
+	}
+	
 }
