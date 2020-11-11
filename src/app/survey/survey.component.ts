459d451be23798f2d6f7fe15eacc79def5cc0bf9
haa96 <54597354+haa96@users.noreply.github.com>
2020-11-10 20:55:49 -0500

    Update survey.component.ts

--- src/app/survey/survey.component.ts
+++ src/app/survey/survey.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+<<<<<<< Updated upstream
+=======
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
+import { MatDialog } from '@angular/material/dialog';
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-survey',
