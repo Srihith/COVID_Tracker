c9335b612788a844a3642e6b3571753656a2ffef
stm102 <32207007+stm102@users.noreply.github.com>
2020-11-16 20:19:23 -0500

    Update me.component.ts

--- src/app/me/me.component.ts
+++ src/app/me/me.component.ts
@@ -162,19 +162,12 @@ openDialogLogin(): void {
       // Define a model for linear regression. 
     this.linearModel = tf.sequential();
     this.linearModel.add(tf.layers.dense({units: 1, inputShape: [1]}));
-
     // Prepare the model for training: Specify the loss and the optimizer.
     this.linearModel.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
-
-
-    // Training data, completely random stuff
     const xs = tf.tensor1d([3.2, 4.4, 5.5]);
     const ys = tf.tensor1d([1.6, 2.7, 3.5]);
-
-
     // Train
     await this.linearModel.fit(xs, ys)
-
     console.log('model trained!')
   }
 
